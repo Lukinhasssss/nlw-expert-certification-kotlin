d336efa781204985d63fef9e8d03f8fc8ce3e7eb
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-02-06 15:47:21 -0300

    build: add spring data jpa and postgres dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    implementation("org.postgresql:postgresql")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
